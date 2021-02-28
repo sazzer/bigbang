0ff7a89934f80b5f3605021ab7995532c0c8449c
Graham Cox <graham@grahamcox.co.uk>
2021-02-28 13:13:24 +0000

    Set up Tracing

--- service/src/main.rs
+++ service/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+#![deny(clippy::all, clippy::pedantic, clippy::nursery)]
+
+use dotenv::dotenv;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::Registry;
+
+#[actix_rt::main]
+async fn main() {
+    dotenv().ok();
+
+    env_logger::init();
+
+    opentelemetry::global::set_text_map_propagator(opentelemetry_jaeger::Propagator::new());
+    let (tracer, _uninstall) = opentelemetry_jaeger::new_pipeline()
+        .with_service_name("bigbang")
+        .from_env()
+        .install()
+        .unwrap();
+    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+    let subscriber = Registry::default().with(telemetry);
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+
+    tracing::debug!("Starting");
 }
