d36e5b7601ee171fe8fac9e132889b002e5ed5c6
Graham Cox <graham@grahamcox.co.uk>
2021-02-28 14:13:37 +0000

    Set up tracing for every incoming HTTP request

--- service/src/server.rs
+++ service/src/server.rs
@@ -1,3 +1,5 @@
+mod span;
+
 use actix_cors::Cors;
 use actix_http::http::header;
 use actix_web::{middleware::Logger, App, HttpServer};
@@ -37,13 +39,17 @@ impl Server {
         HttpServer::new(move || {
             let prometheus = prometheus.clone();
 
-            let app = App::new().wrap(Logger::default()).wrap(prometheus).wrap(
-                Cors::default()
-                    .allow_any_origin()
-                    .allow_any_method()
-                    .allow_any_header()
-                    .expose_headers(vec![header::ETAG, header::LOCATION, header::LINK]),
-            );
+            let app = App::new()
+                .wrap(Logger::default())
+                .wrap(prometheus)
+                .wrap(
+                    Cors::default()
+                        .allow_any_origin()
+                        .allow_any_method()
+                        .allow_any_header()
+                        .expose_headers(vec![header::ETAG, header::LOCATION, header::LINK]),
+                )
+                .wrap(span::Span);
 
             tracing::trace!("Built listener");
 
