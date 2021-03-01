12e680e6dc3fb5e7dc683a12d22781c39bbcbfe5
Graham Cox <graham@grahamcox.co.uk>
2021-03-01 17:44:35 +0000

    Standard Respondable implementation

--- service/src/home/http/get.rs
+++ service/src/home/http/get.rs
@@ -1,5 +1,12 @@
-use crate::http::Response;
+use crate::http::{Response, SimpleRespondable};
+use actix_http::http::{
+    header::{CacheControl, CacheDirective},
+    StatusCode,
+};
 
-pub async fn handle() -> Response<String> {
-    "Hello".to_owned().into()
+pub async fn handle() -> Response<SimpleRespondable<String>> {
+    SimpleRespondable::new("Hello, World".to_owned())
+        .with_status_code(StatusCode::FOUND)
+        .with_header(CacheControl(vec![CacheDirective::NoCache]))
+        .into()
 }
--- service/src/http.rs
+++ service/src/http.rs
@@ -1,5 +1,5 @@
 use actix_http::{
-    http::{HeaderMap, StatusCode},
+    http::{header::Header, HeaderMap, StatusCode},
     Error, Response as HttpResponse,
 };
 use actix_web::Responder;
@@ -32,6 +32,73 @@ where
 
     fn body(self) -> Self::Body {
         self
+    }
+}
+
+/// Simple implementation of the Respondable trait.
+pub struct SimpleRespondable<T>
+where
+    T: Serialize,
+{
+    status_code: StatusCode,
+    headers: HeaderMap,
+    body: T,
+}
+
+impl<T> SimpleRespondable<T>
+where
+    T: Serialize,
+{
+    /// Create a new instance of the `SimpleRespondable` struct wrapping the provided body
+    pub fn new(body: T) -> Self {
+        Self {
+            status_code: StatusCode::OK,
+            headers: HeaderMap::new(),
+            body,
+        }
+    }
+
+    /// Specify the status code to use
+    pub fn with_status_code(mut self, status_code: StatusCode) -> Self {
+        self.status_code = status_code;
+        self
+    }
+
+    /// Specify a header to use
+    pub fn with_header<H>(mut self, header: H) -> Self
+    where
+        H: Header,
+    {
+        let name = H::name();
+        match header.try_into() {
+            Ok(value) => {
+                self.headers.append(name, value);
+            }
+            Err(_) => {
+                tracing::error!(name = ?name, "Failed to process header");
+            }
+        };
+
+        self
+    }
+}
+
+impl<T> Respondable for SimpleRespondable<T>
+where
+    T: Serialize,
+{
+    type Body = T;
+
+    fn status_code(&self) -> StatusCode {
+        self.status_code
+    }
+
+    fn headers(&self) -> HeaderMap {
+        self.headers.clone()
+    }
+
+    fn body(self) -> Self::Body {
+        self.body
     }
 }
 
