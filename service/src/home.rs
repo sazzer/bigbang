0b0a315942be648d105cf388136c722cce217133
Graham Cox <graham@grahamcox.co.uk>
2021-03-03 08:12:19 +0000

    Able to contribute links to the home document

--- service/src/home.rs
+++ service/src/home.rs
@@ -1,2 +1,5 @@
 pub(super) mod component;
 mod http;
+pub(super) mod service;
+
+pub use service::*;
--- service/src/home/component.rs
+++ service/src/home/component.rs
@@ -1,24 +1,42 @@
+use super::{HomeLinksUseCase, LinkContributor};
+use crate::server::Configurer;
+use actix_web::web::ServiceConfig;
 use std::sync::Arc;
 
-use actix_web::web::ServiceConfig;
-
-use crate::server::Configurer;
-
 /// Component representing the home document
-pub struct Component {}
+pub struct Component {
+    service: Arc<HomeLinksUseCase>,
+}
 
+/// Builder to build the home document component
 #[derive(Default)]
-pub struct Builder {}
+pub struct Builder {
+    contributors: Vec<Arc<dyn LinkContributor>>,
+}
 
 impl Builder {
-    #[allow(clippy::unused_self)]
+    /// Add a new contributor of links to the home document.
+    #[allow(dead_code)]
+    pub fn with_contributor(mut self, contributor: Arc<dyn LinkContributor>) -> Self {
+        self.contributors.push(contributor);
+
+        self
+    }
+
+    /// Build the actual home document component.
     pub fn build(self) -> Arc<Component> {
-        Arc::new(Component {})
+        let mut contributors = self.contributors;
+        contributors.push(Arc::new(vec![("self".to_owned(), "/".into())]));
+
+        let service = Arc::new(HomeLinksUseCase { contributors });
+
+        Arc::new(Component { service })
     }
 }
 
 impl Configurer for Component {
     fn configure_server(&self, config: &mut ServiceConfig) {
+        config.data(self.service.clone());
         super::http::configure_server(config);
     }
 }
